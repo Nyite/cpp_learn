7764916116b932b876beb3db1becef29120c6583
Nyite <nazarovpe.work@gmail.com>
2022-07-16 13:31:46 +0300

    Template for problems from chapter 6

---
+++ Prata/chapter_6_problems.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+void task_5();
+void task_6();
+void task_7();
+void task_8();
+void task_9();
+void task_10();
+
+int main ()
+{
+    task_1();
+    // task_2();
+    // task_3();
+    // task_4();
+    // task_5();
+    // task_6();
+    // task_7();
+    // task_8();
+    // task_9();
+    // task_10();
+    return 0;
+}
+
+void task_1()
+{
+    cout.setf(ios::boolalpha);
+    cout << "That's an and: " << ((1 < 3) and (5 > 3)) << "!\n";
+}
+
+void task_2()
+{
+
+}
+
+void task_3()
+{
+
+}
+
+void task_4()
+{
+
+}
+
+void task_5()
+{
+
+}
+
+void task_6()
+{
+
+}
+
+void task_7()
+{
+
+}
+
+void task_8()
+{
+
+}
+
+void task_9()
+{
+
+}
+
+void task_10()
+{
+
+}
